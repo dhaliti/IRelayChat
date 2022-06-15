ef68e7df208f2394dde7cb086f6e9c7808a95a16
dhaliti <damirhaliti@icloud.com>
2022-06-15 12:08:34 +0200

    Concatenation OK

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: dhaliti <dhaliti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 15:22:25 by dhaliti           #+#    #+#             */
-/*   Updated: 2022/06/15 11:20:59 by dhaliti          ###   ########.fr       */
+/*   Updated: 2022/06/15 12:08:03 by dhaliti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,26 +44,38 @@ void send_all(int es)
             send(i, bufWrite, strlen(bufWrite), 0);
 }
 
+static void checkArgs(int &ac, char **av, int &port, string &password)
+{
+	if (ac != 3)
+	{
+        cerr << "Wrong number of arguments\n" << "Usage: ./ircserv port password\n";
+		exit(1);
+	}
+	int port = atoi(av[1]);
+	password = string(av[2]);
+	if (port <= 0)
+	{
+		cerr << "Port cannot be negative !\n";
+		exit(1);
+	}
+}
+
 int main(int ac, char **av)
 {
-    if (ac != 3)
-	{
-        cerr << "Wrong number of arguments\n";
-		cerr << "Usage: ./ircserv port password\n";
-        return (1);
-    }
-
+	int port;
+	string password;
 	Client clients[1024];
-    int port = atoi(av[1]);
-	string password = string(av[2]);
 
+	check_args(ac, av, port, password);
     bzero(&clients, sizeof(clients));
     FD_ZERO(&active);
 
     int serverSock = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSock < 0)
         fatal_error();
-	fcntl(serverSock, F_SETFL, O_NONBLOCK);
+	if (fcntl(serverSock, F_SETFL, O_NONBLOCK) == -1)
+		fatal_error();
+
 
     _max = serverSock;
     FD_SET(serverSock, &active);
@@ -97,18 +109,20 @@ int main(int ac, char **av)
                 clients[clientSock].id = next_id++;
 
                 FD_SET(clientSock, &active);
-                // sprintf(bufWrite, "server: client %d just arrived\n", clients[clientSock].id);
-                // send_all(clientSock);
+                cout << "Client #" << clients[clientSock].id << " just arrived";
                 break ;
             }
 
             if (FD_ISSET(index, &readyRead) && index != serverSock)
 			{
-                int res = recv(index, bufRead, 1024, 0);
+				int res;
+				string cmd;
+				memset(bufRead, '\0', 1024);
+				res = recv(index, bufRead, 1024, 0);
+				cmd += bufRead;
 				if (res <= 0)
 				{
 					string message = ":irc.serv client " + to_string(index) + " just left!\n";
-					cout << "res < 0\n";
 					for (int i = 0; i < 1024; i++)
 						send(i, message.c_str(), message.size(), 0);
 					FD_CLR(index, &active);
@@ -120,19 +134,13 @@ int main(int ac, char **av)
 				}
 				else
 				{
-					string cmd;
-					cmd += bufRead;
-					while (bufRead[res - 1] != '\n')
+					while (!strstr(bufRead, "\n"))
 					{
-						cout << "IN\n";
+						memset(bufRead, '\0', 1024);
 						res = recv(index, bufRead, 1024, 0);
-						cout << "res: " << res << endl;
 						cmd += bufRead;
-						//memset(bufRead, 0, 1024);
-						cout << "IN2\n";
 					}
-					cout << "OUT\n";
-					memset(bufRead, 0, 1024);
+				}
 					if (clients[index].id == index)
 					{
 						const char *cmd2 = cmd.c_str();
@@ -141,31 +149,6 @@ int main(int ac, char **av)
 					else
 						newClient(clients, index);
                     break;
-				}
-				//bufRead[strlen(bufRead)] = 0;
-                //if (res <= 0)
-				// {
-				// 	string message = ":irc.serv client " + to_string(index) + " just left!\n";
-				// 	for (int i = 0; i < 1024; i++)
-				// 		send(i, message.c_str(), message.size(), 0);
-                //     FD_CLR(index, &active);
-				// 	clients[index].id = -1;
-				// 	clients[index].nickname = "";
-				// 	clients[index].username = "";
-                //     close(index);
-                //     break ;
-                // }
-                // else
-				// {
-					// if (clients[index].id == index)
-					// {
-					// 	const char *cmd2 = cmd.c_str();
-					// 	ft_commands(clients, index, cmd2, password);
-					// }
-					// else
-					// 	newClient(clients, index);
-                    // break;
-            //    }
             }
         }
     }
